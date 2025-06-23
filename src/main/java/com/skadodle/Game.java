258e75ce8cc0899203617a10b60bc47fe4aed675
skadodle <slavikkop@gmail.com>
2025-06-24 01:08:48 +0300

    Remove unnecessary method in Game.

--- src/main/java/com/skadodle/Game.java
+++ src/main/java/com/skadodle/Game.java
@@ -12,26 +12,7 @@ public class Game {
         this.players = players;
     }
 
-    public byte[] convertToBytes() {
-        int playersSize = 0;
-        for (Player player : players) {
-            playersSize += player.getByteSize();
-        }
-
-        byte[] data = new byte[4 + 4 + playersSize];
-
-        int pos = 0;
-        System.arraycopy(Constants.intToByteArray(frameTimeout), 0, data, pos, 4);
-        pos += 4;
-        System.arraycopy(Constants.intToByteArray(countOfPlayers), 0, data, pos, 4);
-        pos += 4;
-
-        for (Player player : players) {
-            int playerByteSize = player.getByteSize();
-            System.arraycopy(player.convertToBytes(), 0, data, pos, playerByteSize);
-            pos += playerByteSize;
-        }
-
-        return data;
+    public void startGame(Map map) {
+        System.out.println("Starting game...");
     }
 }
